050db56e03de85fcfb3a85e6eb0a25e006398fb4
Sahan <sahan.ceylonsystems.com>
2017-07-28 16:14:15 +0530

    Change class headers

--- src/Source/ValidatorItem.java
+++ src/Source/ValidatorItem.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Source;
 
 /**
- *
- * @author CS003
+ * @author Sahan Dissanayake (Disapamok)
+ * www.sahan.xyz
+ * https://github.com/disapamok
+ * https://twitter.com/disapamok
  */
 public class ValidatorItem {
 
